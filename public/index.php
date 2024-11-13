76926079ad563e342601bf54099d550c99a615dd
Devanandhan <d.codandabany@gmail.com>
2024-11-13 20:22:42 +0100

    idfilm, idCast

--- public/index.php
+++ public/index.php
@@ -6,9 +6,30 @@ use Narut\Projectphp\DBAL\Connector;
 use Narut\Projectphp\Entity\Cast;
 
 $connection = new Connector();
+$castEntity = new Cast($connection->dbh);
 
+// Vérifie et récupère `id_film` et `id_cast` depuis $_GET, en les validant
+$idFilm = isset($_GET['id_film']) && ctype_digit($_GET['id_film']) ? (int)$_GET['id_film'] : null;
+$idCast = isset($_GET['id_cast']) && ctype_digit($_GET['id_cast']) ? (int)$_GET['id_cast'] : null;
 
+// Récupère le cast filtré si les IDs sont fournis, sinon récupère tout le cast
+$cast = $castEntity->getCastByFilmAndCastId($idFilm, $idCast);
 
-require_once __DIR__ . '/../templates\base.html.php';
+require_once __DIR__ . '/../templates/base.html.php';
+?>  
 
-?>
+<ul>
+    <?php foreach ($cast as $filmCast) { ?>
+        <li>First Name: <?= $filmCast['firstName'] ?></li>
+        <li>Last Name: <?= $filmCast['lastName'] ?></li>
+
+        <!-- Affiche l'ID Film et l'ID Cast si présents dans la base de données -->
+        <?php if (isset($filmCast['idFilm'])) { ?>
+            <li>ID Film: <?= $filmCast['idFilm'] ?></li>
+        <?php } ?>
+
+        <?php if (isset($filmCast['idCast'])) { ?>
+            <li>ID Cast: <?= $filmCast['idCast'] ?></li>
+        <?php } ?>
+    <?php } ?>
+</ul>
--- src/Entity/Cast.php
+++ src/Entity/Cast.php
@@ -10,8 +10,38 @@ class Cast {
     {
         $this->connector = $connector;
     }
+
+    public function getCast(): array {
+        // Récupère le cast complet avec tous les firstName et lastName
+        $sql = "SELECT * FROM cast";
+        $statement = $this->connector->prepare($sql);
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
     
+    public function getCastByFilmAndCastId($idFilm = null, $idCast = null): array {
+        // Construit la requête SQL en fonction des filtres idFilm et idCast
+        $sql = "SELECT * FROM cast WHERE 1=1"; // 1=1 pour simplifier l'ajout des conditions
+        $params = [];
+
+        if ($idFilm !== null) {
+            $sql .= " AND idFilm = :idFilm";
+            $params[':idFilm'] = $idFilm;
+        }
+
+        if ($idCast !== null) {
+            $sql .= " AND idCast = :idCast";
+            $params[':idCast'] = $idCast;
+        }
+
+        $statement = $this->connector->prepare($sql);
+        foreach ($params as $key => $value) {
+            $statement->bindValue($key, $value, \PDO::PARAM_INT);
+        }
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
 }
-
-
 ?>
